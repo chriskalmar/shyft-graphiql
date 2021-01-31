8aaed368af7bf7aa172f48bfc734f7428524b7f3
Chris Kalmar <christian.kalmar@gmail.com>
2021-01-31 23:19:10 +0100

    set theming up

---
+++ src/theme.js
@@ -0,0 +1,10 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+import { red } from '@material-ui/core/colors';
+
+// Create a theme instance.
+const theme = createMuiTheme({
+  // palette: {
+  // },
+});
+
+export default theme;
