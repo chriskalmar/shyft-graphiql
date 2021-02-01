2cf451e430f0c0692d3e8d6eb4454f5295d07c4b
Chris Kalmar <christian.kalmar@gmail.com>
2021-02-01 13:13:51 +0100

    implement custom data fetcher to handle re-authentication with a refresh token on failed requests

---
+++ src/fetcher.js
@@ -0,0 +1,55 @@
+import axios from 'axios';
+import { getToken, refreshToken } from './auth';
+
+export const axiosInstance = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:4000/graphql',
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
+  },
+});
+
+axiosInstance.interceptors.request.use(
+  (config) => {
+    if (!config.isRefreshTokenRequest) {
+      const token = getToken();
+
+      if (token) {
+        config.headers = config.headers || {};
+        config.headers.Authorization = `JWT ${token}`;
+      }
+    }
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
+);
+
+axiosInstance.interceptors.response.use(undefined, (err) => {
+  const res = err.response;
+
+  if (
+    res.status === 401 &&
+    res.data?.errors?.[0]?.code === 'AuthorizationTokenExpiredError' &&
+    !res.config?.__isRetryRequest
+  ) {
+    return refreshToken(fetcher).then(() => {
+      res.config.__isRetryRequest = true;
+      return axiosInstance(res.config);
+    });
+  }
+
+  throw err;
+});
+
+export const fetcher = (...params) =>
+  axiosInstance
+    .post('', ...params)
+    .then((res) => {
+      return res.data;
+    })
+    .catch((err) => {
+      throw err.response.data;
+    });
