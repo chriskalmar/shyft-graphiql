e280388e5f40fa462d21dfe8ceeb21bbb0cfd6b0
Chris Kalmar <christian.kalmar@gmail.com>
2021-02-01 13:14:33 +0100

    handle logic of authentication and interacting with localStorage

--- src/auth.js
+++ src/auth.js
@@ -11,12 +11,19 @@ export const getToken = () => {
   }
 };
 
+export const getRefreshToken = () => {
+  if (typeof window !== 'undefined') {
+    return window.localStorage.getItem('refreshToken');
+  }
+};
+
 export const authenticate = async (fetcher, username, password) => {
   let success = false;
   let error;
 
-  return fetcher({
-    query: `
+  return (
+    fetcher({
+      query: `
         mutation authenticateByUsername($username: String!, $password: String!) {
           authenticateByUsername(
             input: {
@@ -36,34 +43,77 @@ export const authenticate = async (fetcher, username, password) => {
           }
         }
       `,
-    variables: {
-      username,
-      password,
-    },
-    authMode: false,
-  })
-    .then((result) => {
-      if (result.errors?.[0]?.code === 'AuthenticationError') {
-        error = 'AuthenticationError';
-      } else {
-        const token =
-          result.data?.authenticateByUsername?.result?.tokens?.token;
-        const refreshToken =
-          result.data?.authenticateByUsername?.result?.tokens?.refreshToken;
+      variables: {
+        username,
+        password,
+      },
+    })
+      // .then((res) => res.data)
+      .then((result) => {
+        if (result.errors?.[0]?.code === 'AuthenticationError') {
+          error = 'AuthenticationError';
+        } else {
+          const token =
+            result.data?.authenticateByUsername?.result?.tokens?.token;
+          const refreshToken =
+            result.data?.authenticateByUsername?.result?.tokens?.refreshToken;
 
-        if (token && refreshToken) {
-          if (typeof window !== 'undefined') {
-            window.localStorage.setItem('token', token);
-            window.localStorage.setItem('refreshToken', refreshToken);
-            success = true;
+          if (token && refreshToken) {
+            if (typeof window !== 'undefined') {
+              window.localStorage.setItem('token', token);
+              window.localStorage.setItem('refreshToken', refreshToken);
+              success = true;
+            }
           }
         }
-      }
 
-      return [success, error];
-    })
-    .catch((err) => {
-      console.error(err);
-      return [success, 'NetworkError'];
-    });
+        return [success, error];
+      })
+      .catch((err) => {
+        console.error(err);
+        return [success, 'NetworkError'];
+      })
+  );
+};
+
+export const refreshToken = async (fetcher) => {
+  const refreshToken = getRefreshToken();
+
+  if (!refreshToken) {
+    throw new Error('No refreshToken found!');
+  }
+
+  return (
+    fetcher(
+      {
+        query: `
+        mutation refreshToken($refreshToken: String!) {
+          refreshToken(input: {
+            data:{
+              refreshToken: $refreshToken
+            }
+          }){
+            result {
+              token
+            }
+          }
+        }
+      `,
+        variables: {
+          refreshToken,
+        },
+      },
+      { isRefreshTokenRequest: true },
+    )
+      // .then((res) => res.data)
+      .then((result) => {
+        const token = result.data?.refreshToken?.result?.token;
+
+        if (token) {
+          if (typeof window !== 'undefined') {
+            window.localStorage.setItem('token', token);
+          }
+        }
+      })
+  );
 };
